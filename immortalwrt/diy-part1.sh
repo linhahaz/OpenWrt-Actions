7cf2c5b5474b4d594794a381d90174eede20c040
linhahaz <49563001+linhahaz@users.noreply.github.com>
2024-11-01 17:48:07 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -16,15 +16,8 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # rm -rf target/linux/ramips
 # svn export https://github.com/padavanonly/immortalwrt/trunk/target/linux/ramips target/linux/ramips
-
-# 修改系统版本（界面显示）
-VERSION=${GITHUB_WORKSPACE}/immortalwrt/version
-VERSION_TEXT=$(head -n 1 ${VERSION} | tr -d ' \r\n')
-if [ -n "$VERSION_TEXT" ]; then
-	sed -i "/^VERSION_NUMBER:=.*SNAPSHOT/s/SNAPSHOT/${VERSION_TEXT}/" include/version.mk
-fi
