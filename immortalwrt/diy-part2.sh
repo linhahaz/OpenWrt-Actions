412d8951785ee66d3dd9a5b75cfa3ac6b9be7edc
linhahaz <49563001+linhahaz@users.noreply.github.com>
2024-11-01 15:49:42 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 默认IP由1.1修改为0.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Add luci-app
