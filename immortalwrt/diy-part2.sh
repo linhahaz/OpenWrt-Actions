375a0988769aee5007ba60371e1ce0c237f0e653
linhahaz <49563001+linhahaz@users.noreply.github.com>
2024-11-10 19:58:01 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -24,7 +24,7 @@ sed -i 's/192.168.1.1/192.168.5.8/g' package/base-files/files/bin/config_generat
 # merge_package main https://github.com/sbwml/luci-theme-argon feeds/luci/themes luci-theme-argon
 
 # 最大连接数修改为554288
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=554288' package/base-files/files/etc/sysctl.conf
+# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=554288' package/base-files/files/etc/sysctl.conf
 
 # 修复上移下移按钮翻译
 # sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
@@ -46,7 +46,7 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=554288' packag
 # git clone https://github.com/sbwml/package_network_services_ppp package/network/services/ppp
 
 # 精简 UPnP 菜单名称
-sed -i 's#\"title\": \"UPnP IGD \& PCP/NAT-PMP\"#\"title\": \"UPnP\"#g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
+# sed -i 's#\"title\": \"UPnP IGD \& PCP/NAT-PMP\"#\"title\": \"UPnP\"#g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
 
 # 修复编译时提示 freeswitch 缺少 libpcre 依赖
 # sed -i 's/+libpcre \\$/+libpcre2 \\/g' package/feeds/telephony/freeswitch/Makefile
